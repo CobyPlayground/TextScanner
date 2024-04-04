c3dfff17610b57b746ad76d13332f1eb125dad81
coby <coby5502@gmail.com>
2024-04-04 10:44:46 +0900

    텍스트 스캐너

---
+++ TextScanner/CameraView.swift
@@ -0,0 +1,100 @@
+//
+//  CameraView.swift
+//  TextScanner
+//
+//  Created by Coby Kim on 4/4/24.
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+
+struct CameraView: UIViewControllerRepresentable {
+    
+    @Binding var image: UIImage?
+    
+    typealias UIViewControllerType = UIImagePickerController
+    
+    func makeUIViewController(context: Context) -> UIViewControllerType {
+        let viewController = UIViewControllerType()
+        viewController.delegate = context.coordinator
+        viewController.sourceType = .camera
+        viewController.cameraDevice = .rear
+        viewController.cameraOverlayView = .none
+        
+        return viewController
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        
+    }
+    
+    func makeCoordinator() -> CameraView.Coordinator {
+        return Coordinator(self)
+    }
+}
+
+extension CameraView {
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        var parent: CameraView
+        
+        init(_ parent: CameraView) {
+            self.parent = parent
+        }
+        
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            // Dismiss the picker controller
+            picker.dismiss(animated: true, completion: nil)
+        }
+        
+        // Resize the image to a specified target size
+        func resizeImage(image: UIImage, targetSize: CGSize) -> UIImage {
+            let size = image.size
+            
+            let widthRatio  = targetSize.width  / size.width
+            let heightRatio = targetSize.height / size.height
+            
+            // Determine what our orientation is, and use that to form the rectangle
+            var newSize: CGSize
+            if(widthRatio > heightRatio) {
+                newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
+            } else {
+                newSize = CGSize(width: size.width * widthRatio,  height: size.height * widthRatio)
+            }
+            
+            // This is the rect that we've calculated out and this is what is actually used below
+            let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
+            
+            // Actually do the resizing to the rect using the ImageContext stuff
+            UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+            image.draw(in: rect)
+            let newImage = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            
+            return newImage!
+        }
+        
+        // Override the didFinishPickingMediaWithInfo function
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+                // Resize the image if needed, or just assign it directly to parent.image
+                // For example, to resize the image to 3000x2250:
+                let targetSize = CGSize(width: 3000, height: 2250)
+                let resizedImage = resizeImage(image: image, targetSize: targetSize)
+                
+                self.parent.image = resizedImage
+                
+                // Dismiss the picker controller
+                picker.dismiss(animated: true, completion: nil)
+            }
+        }
+    }
+}
+
+extension UIImage {
+    func imageResized(to size: CGSize) -> UIImage {
+        return UIGraphicsImageRenderer(size: size).image { _ in
+            draw(in: CGRect(origin: .zero, size: size))
+        }
+    }
+}
--- TextScanner/ContentView.swift
+++ TextScanner/ContentView.swift
@@ -6,19 +6,82 @@
 //
 
 import SwiftUI
+import Vision
 
 struct ContentView: View {
+    
+    @State private var cameraOpen = false
+    @State private var imageTaken: UIImage?
+    @State private var recognizedTexts = [String]()
+    @State private var isLoading = false
+    
+    func recognizeCardText() {
+        print("reading text ")
+        
+        let requestHandler = VNImageRequestHandler(cgImage: self.imageTaken!.cgImage!)
+        
+        let recognizeTextRequest = VNRecognizeTextRequest { (request, error) in
+            // 1. Parse the results
+            guard let observations = request.results as? [VNRecognizedTextObservation] else { return }
+            
+            // 2. Extract the data you want
+            for observation in observations {
+                let recognizedText = observation.topCandidates(1).first!.string
+                
+                self.recognizedTexts.append(recognizedText)
+            }
+        }
+        
+        DispatchQueue.global(qos: .userInitiated).async {
+            do {
+                try requestHandler.perform([recognizeTextRequest])
+                
+                self.isLoading = false
+            } catch {
+                print(error)
+            }
+        }
+    }
+    
+    var pictureTakenView: some View {
+        VStack {
+            Image(uiImage: self.imageTaken!)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 200, height: 200)
+            
+            Button(action: {
+                self.imageTaken = nil
+                self.recognizedTexts = [String]()
+            }, label: {
+                HStack {
+                    Image(systemName: "camera")
+                    Text("Re-take picture")
+                }
+            })
+            
+            List {
+                ForEach(self.recognizedTexts, id: \.self) {
+                    Text("\($0)")
+                }
+            }
+        }
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if self.imageTaken == nil {
+                CameraView(image: self.$imageTaken)
+            } else {
+                if !self.isLoading {
+                    self.pictureTakenView
+                        .onAppear {
+                            self.recognizeCardText()
+                        }
+                } else {
+                    ProgressView()
+                }
+            }
         }
-        .padding()
     }
-}
-
-#Preview {
-    ContentView()
 }
