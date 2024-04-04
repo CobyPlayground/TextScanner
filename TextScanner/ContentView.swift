956502f54d0e3654827579c63acc1aa42fea46bf
coby <coby5502@gmail.com>
2024-04-04 10:11:08 +0900

    Initial Commit

---
+++ TextScanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TextScanner
+//
+//  Created by Coby Kim on 4/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TextScanner/TextScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextScannerApp.swift
+//  TextScanner
+//
+//  Created by Coby Kim on 4/4/24.
+//
+
+import SwiftUI
+
+@main
+struct TextScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
