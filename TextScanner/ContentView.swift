693a0d48b4a20a6d2cf8ac6a3d7a4bf8fb4afd47
coby <coby5502@gmail.com>
2024-04-04 10:57:21 +0900

    카드 인식 추가

--- TextScanner/ContentView.swift
+++ TextScanner/ContentView.swift
@@ -15,6 +15,10 @@ struct ContentView: View {
     @State private var recognizedTexts = [String]()
     @State private var isLoading = false
     
+    // 카드 스캔
+    @State private var cardNumber: String = ""
+    @State private var validDate: String = ""
+    
     func recognizeCardText() {
         print("reading text ")
         
@@ -27,15 +31,21 @@ struct ContentView: View {
             // 2. Extract the data you want
             for observation in observations {
                 let recognizedText = observation.topCandidates(1).first!.string
-                
                 self.recognizedTexts.append(recognizedText)
+                
+                if self.isCardFormat(recognizedText) {
+                    self.cardNumber = recognizedText
+                }
+                
+                if self.isValidDate(recognizedText) {
+                    self.validDate = recognizedText
+                }
             }
         }
         
         DispatchQueue.global(qos: .userInitiated).async {
             do {
                 try requestHandler.perform([recognizeTextRequest])
-                
                 self.isLoading = false
             } catch {
                 print(error)
@@ -52,6 +62,8 @@ struct ContentView: View {
             
             Button(action: {
                 self.imageTaken = nil
+                self.cardNumber = ""
+                self.validDate = ""
                 self.recognizedTexts = [String]()
             }, label: {
                 HStack {
@@ -61,9 +73,11 @@ struct ContentView: View {
             })
             
             List {
-                ForEach(self.recognizedTexts, id: \.self) {
-                    Text("\($0)")
-                }
+//                ForEach(self.recognizedTexts, id: \.self) {
+//                    Text("\($0)")
+//                }
+                Text("카드번호: \(self.cardNumber)")
+                Text("유효기간: \(self.validDate)")
             }
         }
     }
@@ -83,5 +97,21 @@ struct ContentView: View {
                 }
             }
         }
+    }
+    
+    // 카드번호 판단
+    func isCardFormat(_ text: String) -> Bool {
+        let pattern = "^\\d{4} \\d{4} \\d{4} \\d{4}$"
+        let regex = try! NSRegularExpression(pattern: pattern)
+        let matches = regex.matches(in: text, range: NSRange(location: 0, length: text.utf16.count))
+        return matches.count > 0
+    }
+    
+    // 유효기간 판단
+    func isValidDate(_ text: String) -> Bool {
+        let pattern = "^\\d{2}/\\d{2}$"
+        let regex = try! NSRegularExpression(pattern: pattern)
+        let matches = regex.matches(in: text, range: NSRange(location: 0, length: text.utf16.count))
+        return matches.count > 0
     }
 }
--- TextScanner/TextScannerApp.swift
+++ TextScanner/TextScannerApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct TextScannerApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+        ContentView()
         }
     }
 }
